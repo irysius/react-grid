caed589a2f424647c01c1c00e6929c90d6ccd1a1
Sy <p122ed7@dayforce.com>
2016-12-08 13:55:17 -0500

    definitions

--- js/App.tsx
+++ js/App.tsx
@@ -1,5 +1,20 @@
-class App extends React.Component<any, any> {
+interface IPeople {
+    id: number;
+    first_name: string;
+    last_name: string;
+    email: string;
+    gender: 'Male'|'Female';
+    ip_address: string;
+}
+interface IPeopleData {
+    people: IPeople[];
+}
+
+class App extends React.Component<IPeopleData, any> {
+    constructor(props) {
+        super(props);
+    }
     render() {
-        return <Table />
+        return <Table {...this.props} />;
     }
 }

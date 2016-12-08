4443aeac1e791d267e32991482752eade541c275
mike <mikeguo1992@gmail.com>
2016-12-08 14:38:58 -0500

    classname

--- js/Row.tsx
+++ js/Row.tsx
@@ -5,13 +5,13 @@
 class Row extends React.Component<IPeople, any> {
     render() {
         return (
-            <div class="row">
-                <div class="col-md-1">{this.props.id}</div>
-                <div class="col-md-2">{this.props.first_name}</div>
-                <div class="col-md-2">{this.props.last_name}</div>
-                <div class="col-md-3">{this.props.email}</div>
-                <div class="col-md-1">{this.props.gender}</div>
-                <div class="col-md-3">{this.props.ip_address}</div>
+            <div className="row">
+                <div className="col-md-1">{this.props.id}</div>
+                <div className="col-md-2">{this.props.first_name}</div>
+                <div className="col-md-2">{this.props.last_name}</div>
+                <div className="col-md-3">{this.props.email}</div>
+                <div className="col-md-1">{this.props.gender}</div>
+                <div className="col-md-3">{this.props.ip_address}</div>
             </div>
         )
     }

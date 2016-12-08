9d8ea51dc5fda310c57e47c62b902966c1c82dca
mike <mikeguo1992@gmail.com>
2016-12-08 12:24:34 -0500

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,14 @@
 var gulp = require('gulp');
 
 var react = require('gulp-react');
+
+gulp.task('compile-jsx', function() {
+	gulp.src(['/jsx/**/*.jsx'])
+		.pipe(react())
+        .pipe(gulp.dest('/js'));
+});
+
+gulp.task('default', function() {
+
+	gulp.watch(['/jsx/**/*.jsx'], ['compile-jsx']);
+});
---
+++ jsx/App.jsx
@@ -0,0 +1,6 @@
+class App extends React.Component {
+    render() {
+        return <h1>Hello, {this.props.name}</h1> 
+    }
+}
+// asdfaasdfas

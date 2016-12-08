e4ba4f5683dbc92d6655d23cf4a7160b62cf1761
Sy <p122ed7@dayforce.com>
2016-12-08 13:47:03 -0500

    scaffolds

--- js/App.tsx
+++ js/App.tsx
@@ -1,6 +1,5 @@
 class App extends React.Component<any, any> {
     render() {
-        return (<div>Hello, {this.props.name} stuff</div>);
+        return <Table />
     }
-
 }
---
+++ js/Table.tsx
@@ -0,0 +1,5 @@
+class Table extends React.Component<any, any> {
+    render() {
+        return <div>This is a table</div>;
+    }
+}
